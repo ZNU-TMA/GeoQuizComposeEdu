790296100e445762bed65c7021f9e8527c8bb616
Hennadii Kolomoiets <kgp@znu.edu.ua>
2025-10-02 22:11:49 +0300

    Add questionsMutableStateFlow variable and getAllQuestions() and removeQuestion(question: Question) functions that use MutableStateFlow<List<Question>>.

--- app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionRepository.kt
+++ app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionRepository.kt
@@ -1,5 +1,9 @@
 package ua.edu.znu.geoquizcomposeedu.data
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
 class QuestionRepository(val questionDataSource: QuestionDataSource){
 
     fun getQuestionByIndex(index: Int): Question {
@@ -8,4 +12,26 @@ class QuestionRepository(val questionDataSource: QuestionDataSource){
 
     fun getQuestionBankSize() = questionDataSource.getQuestionBank().size
 
+    private val questionsMutableStateFlow: MutableStateFlow<List<Question>> =
+        MutableStateFlow(questionDataSource.getQuestionBank())
+
+    fun getAllQuestions(): StateFlow<List<Question>> {
+        return questionsMutableStateFlow
+    }
+
+    fun removeQuestion(question: Question) {
+        questionsMutableStateFlow.update { oldQuestions -> oldQuestions - question }
+    }
+
+//    fun addQuestion(question: Question) {
+//        questionsMutableStateFlow.update { oldQuestions -> oldQuestions + question }
+//    }
+//
+//    fun updateQuestion(updatedQuestion: Question) {
+//        questionsMutableStateFlow.update { oldQuestions ->
+//            oldQuestions.map {
+//                if (it.id == updatedQuestion.id) updatedQuestion else it
+//            }
+//        }
+//    }
 }
