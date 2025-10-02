3f1c16d0793994f66966d708336ae609412c70a3
Hennadii Kolomoiets <kgp@znu.edu.ua>
2025-10-02 22:09:12 +0300

    Replace getQuestionBank() function result type from List<Question> to MutableList<Question> to allow removal/addition/update of questions.

--- app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionDataSource.kt
+++ app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionDataSource.kt
@@ -4,12 +4,13 @@ import ua.edu.znu.geoquizcomposeedu.R
 
 class QuestionDataSource {
 
-    fun getQuestionBank(): List<Question> = listOf(
-        Question(textResId = R.string.question_australia, answer = true),
-        Question(textResId = R.string.question_oceans, answer = true),
-        Question(textResId = R.string.question_mideast, answer = false),
-        Question(textResId = R.string.question_africa, answer = false),
-        Question(textResId = R.string.question_americas, answer = true),
-        Question(textResId = R.string.question_asia, answer = true)
+    // Now list is mutable to allow removal/addition/update of questions
+    fun getQuestionBank() = mutableListOf(
+        Question(1, textResId = R.string.question_australia, answer = true),
+        Question(2, textResId = R.string.question_oceans, answer = true),
+        Question(3, textResId = R.string.question_mideast, answer = false),
+        Question(4, textResId = R.string.question_africa, answer = false),
+        Question(5, textResId = R.string.question_americas, answer = true),
+        Question(6, textResId = R.string.question_asia, answer = true)
     )
 }
