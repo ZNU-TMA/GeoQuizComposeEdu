83149c5ee72b05a41b2d45e0e9586135f6238056
Hennadii Kolomoiets <kgp@znu.edu.ua>
2025-10-12 20:41:27 +0300

    QuestionRepository refactored to add add/update/remove question functions. QuestionListViewModel and QuestionListScreen were added with string resources.

--- app/src/main/java/ua/edu/znu/geoquizcomposeedu/ui/screens/QuestionListScreen.kt
+++ app/src/main/java/ua/edu/znu/geoquizcomposeedu/ui/screens/QuestionListScreen.kt
@@ -4,23 +4,27 @@ import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -29,78 +33,99 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import ua.edu.znu.geoquizcomposeedu.R
 import ua.edu.znu.geoquizcomposeedu.data.Question
-import ua.edu.znu.geoquizcomposeedu.data.QuestionRepository
+import ua.edu.znu.geoquizcomposeedu.data.QuestionRepositoryImpl
 import ua.edu.znu.geoquizcomposeedu.viewmodel.QuestionListViewModel
+import ua.edu.znu.geoquizcomposeedu.viewmodel.ViewModelFactory
 
 private const val TAG = "QuestionListScreen"
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun QuestionListScreen(
-//    questionListViewModel: QuestionListViewModel,
-//    onQuestionSelected: (Question) -> Unit = {},
     innerPadding: PaddingValues,
 ) {
-    val questionRepository = QuestionRepository.getInstance()
+    val questionRepository = QuestionRepositoryImpl.getInstance()
 
+//    val questionListViewModel: QuestionListViewModel = viewModel(
+//        factory = object : ViewModelProvider.Factory {
+//            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//                if (modelClass.isAssignableFrom(QuestionListViewModel::class.java)) {
+//                    @Suppress("UNCHECKED_CAST")
+//                    return QuestionListViewModel(questionRepository) as T
+//                }
+//                throw IllegalArgumentException("Unknown ViewModel class")
+//            }
+//        }
+//    )
+
+    // Using a generic ViewModelFactory to reduce boilerplate code
     val questionListViewModel: QuestionListViewModel = viewModel(
-        factory = object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                if (modelClass.isAssignableFrom(QuestionListViewModel::class.java)) {
-                    @Suppress("UNCHECKED_CAST")
-                    return QuestionListViewModel(questionRepository) as T
-                }
-                throw IllegalArgumentException("Unknown ViewModel class")
-            }
+        factory = ViewModelFactory(QuestionListViewModel::class.java) {
+            QuestionListViewModel(questionRepository)
         }
     )
-    // Collect the question list state from the ViewModel and return the question list
-    // because of delegate using by.
-    val questionList:List<Question> by questionListViewModel.questionListFlow.collectAsStateWithLifecycle()
+
+    // Use StateFlow in ViewModel to collect question list state as State in Composable.
+    val questionList: List<Question> by questionListViewModel.questionListFlow.collectAsStateWithLifecycle()
 
     Log.d(TAG, "QuestionListScreen: questionList.size = ${questionList.size}")
 
-    LazyColumn(
-        contentPadding = innerPadding,
-    ) {
-        stickyHeader {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.LightGray)
-                    .padding(4.dp),
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Question",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .fillMaxWidth(0.65f)
-                        .padding(start = 8.dp)
-                )
-                Text(
-                    text = "Is true",
-                    fontWeight = FontWeight.Bold,
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            contentPadding = innerPadding,
+        ) {
+            listHeader()
+            items(
+                items = questionList,
+                key = { question -> question.id }
+            ) { question ->
+                QuestionCard(
+                    question,
+                    onQuestionDeleted = {
+                        questionListViewModel.onRemoveQuestionClick(question)
+                    }
                 )
             }
         }
-        items(
-            items = questionList,
-            key = { question -> question.id }
-        ) { question ->
-            QuestionCard(
-                question,
-                onQuestionDeleted = {
-                    questionListViewModel.onRemoveQuestionClick(question)
-                }
+        FloatingActionButton(
+            onClick = { /* TODO: Navigate to add question screen */ },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = stringResource(R.string.add_question))
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+fun LazyListScope.listHeader() {
+    stickyHeader {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .padding(4.dp),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Text(
+                text = "Question",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth(0.65f)
+                    .padding(start = 8.dp)
+            )
+            Text(
+                text = "Is true",
+                fontWeight = FontWeight.Bold,
             )
         }
     }
@@ -158,9 +183,16 @@ fun QuestionCard(
 fun QuestionCardPreview() {
     QuestionCard(
         question = Question(
-            id = 1,
             questionText = "Канберра - це столиця Австралії",
             answer = true
         )
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun QuestionListScreenPreview() {
+    QuestionListScreen(
+        innerPadding = PaddingValues(0.dp)
     )
 }
--- app/src/main/java/ua/edu/znu/geoquizcomposeedu/viewmodel/QuestionListViewModel.kt
+++ app/src/main/java/ua/edu/znu/geoquizcomposeedu/viewmodel/QuestionListViewModel.kt
@@ -1,19 +1,14 @@
 package ua.edu.znu.geoquizcomposeedu.viewmodel
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import ua.edu.znu.geoquizcomposeedu.data.Question
 import ua.edu.znu.geoquizcomposeedu.data.QuestionRepository
 
+// Pass QuestionRepository as a constructor parameter to MainViewModel
+// and assign it to a property.
 class QuestionListViewModel(private val questionRepository: QuestionRepository) : ViewModel() {
     // Expose the question list as a StateFlow
     var questionListFlow = questionRepository.getQuestionListState()
-
-    fun onAddQuestionClick(question: Question) = questionRepository.addQuestion(question)
-
-    fun onUpdateQuestionClick(question: Question) = questionRepository.updateQuestion(question)
 
     fun onRemoveQuestionClick(question: Question) = questionRepository.removeQuestion(question)
 }
