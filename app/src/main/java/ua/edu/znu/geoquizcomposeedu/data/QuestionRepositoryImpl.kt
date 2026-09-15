fa1a5b203d8b21a43a32f8fdc64e723d017c41b9
kolgenpav <kolgenpav@gmail.com>
2026-09-15 19:07:48 +0300

    Refactor QuestionRepositoryImpl.kt to add artificial delay for negative timeout testing.

--- app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionRepositoryImpl.kt
+++ app/src/main/java/ua/edu/znu/geoquizcomposeedu/data/QuestionRepositoryImpl.kt
@@ -3,9 +3,11 @@ package ua.edu.znu.geoquizcomposeedu.data
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlin.time.Duration.Companion.milliseconds
 
 //private const val TAG = "QuestionRepositoryImpl"
 
@@ -71,6 +73,8 @@ class QuestionRepositoryImpl private constructor(private val questionDataSource:
 
     // inside QuestionRepositoryImpl
     override suspend fun addQuestion(question: Question) {
+        // For suspend function use delay instead of Thread.sleep to avoid blocking the main thread
+        delay(100.milliseconds) // Simulate a delay for testing purposes
         // Insert into DB and let the Room Flow emit the changed list
         questionDataSource.addQuestion(question)
         // kept for debugging/logging if needed; do not try to maintain a separate in-memory list
